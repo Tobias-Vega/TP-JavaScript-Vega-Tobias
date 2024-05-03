3903184e3f85cc5006bfd63436258b4577a2e362
Tobias <tobiasvega1210@gmail.com>
2024-05-03 09:58:52 -0300

    se realizo el ejercicio del rango de edad

---
+++ assets/js/rangoEdad.js
@@ -0,0 +1,11 @@
+let edad = parseInt(prompt("Ingrese su edad"))
+
+if (edad >= 0 && edad <= 12) {
+    console.log("usted es un niño")
+} else if (edad >= 13 && edad <=19) {
+    console.log("usted es un adolescente") 
+} else if (edad >= 20 && edad <= 59) {
+    console.log("usted es un adulto")
+} else if (edad >= 60) {
+    console.log("usted es un adulto mayor")
+} else console.log ("el valor ingresado no es valido, por favor ingrese un numero mayor a 0")
